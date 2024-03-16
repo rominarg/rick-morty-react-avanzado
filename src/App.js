33d1cf9e8dd2ec79d910e5c5091e48e23b94cced
RominaNavarrete <rominagnavarrete@gmail.com>
2024-03-16 05:22:17 -0300

    Componets rafce

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import './App.css';
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Ricky and Morty Span . </h1> 
+      <h1 className="text-center">Ricky and Morty</h1> 
       <div class="container">
   <div class="row">
     <div class="col-3">
-      One of three columns
+      <Filters></Filters>
     </div>
     <div class="col-8">
-      One of three columns
+      Cards
     </div>
 
   </div>
